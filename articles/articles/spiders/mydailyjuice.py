1ae772a604e24fe75366e5dabe57066ea495cda3
Joshuatanmy <140237486+Joshuatanmy@users.noreply.github.com>
2026-09-08 17:22:51 +0800

    Added MyDailyJuice spider to crawl mydailyjuice.my website and extract articles data from there. Current implementation has broken pagination logic and bug with article date and topic extraction. Only scrapes information from first page. The date and topic extraction only works if the article contains exactly one topic in the sub-header of the article, and is not dynamic.

--- articles/articles/spiders/mydailyjuice.py
+++ articles/articles/spiders/mydailyjuice.py
@@ -1,4 +1,5 @@
 import scrapy
+from articles.items import ArticlesItem
 
 
 class MydailyjuiceSpider(scrapy.Spider):
@@ -7,4 +8,28 @@ class MydailyjuiceSpider(scrapy.Spider):
     start_urls = ["https://mydailyjuice.my/"]
 
     def parse(self, response):
-        pass
+        for article in response.css("div.dt-col-sm-6"):
+            item = ArticlesItem()
+            item["title"] = article.css("h6 > a::text").get()
+            item["url"] = article.css("div.thumb > a::attr(href)").get()
+            item["author"] = article.css("li:nth-child(1) > a > img::attr(alt)").get()
+
+            article_url = item["url"]
+            if article_url:
+                yield response.follow(url=article_url, callback=self.parse_article, meta={"item": item})
+
+        next_page = response.css("div.nav-links > a.next.page-numbers::attr(href)").get()
+        if next_page:
+            next_page_url = response.urljoin(next_page)
+            self.logger.info(
+                f"Navigating to next page with URL {next_page_url}."
+            )
+            yield scrapy.Request(url=next_page_url, callback=self.parse, errback=self.log_error)
+
+
+    def parse_article(self, response):
+        item = response.meta["item"]
+        item["body"] = response.css('div[class="clearfix"] > p::text, div[class="clearfix"] > a::attr(href)').getall()
+        item["tag"] = response.css('div.post-header > ul > li:nth-child(2) > a[rel="category tag"]::text').getall()
+        item["date"] = response.css("div.post-header > ul > li:nth-child(3)::text").get()
+        yield item
