85ac40c69fd56aca0f77e38e8cf6d92756aefebe
Joshuatanmy <140237486+Joshuatanmy@users.noreply.github.com>
2026-09-09 14:28:07 +0800

    Fixed bug with article URL extraction. Previously extracted URL from thumbnail element, which sometimes doesn't exist. Now extracts from the Title element which should always exist. Fixed bug with next page navigation. No longer yields a request to the article page, now just calls the function to parse the next page, and lets the function read and update values of the articlesitem object. Function still yields item back to caller, left in case of future functionality changes.

--- articles/articles/spiders/mydailyjuice.py
+++ articles/articles/spiders/mydailyjuice.py
@@ -11,12 +11,13 @@ class MydailyjuiceSpider(scrapy.Spider):
         for article in response.css("div.dt-col-sm-6"):
             item = ArticlesItem()
             item["title"] = article.css("h6 > a::text").get()
-            item["url"] = article.css("div.thumb > a::attr(href)").get()
+            item["url"] = article.css("h6 > a::attr(href)").get()
             item["author"] = article.css("li:nth-child(1) > a > img::attr(alt)").get()
 
             article_url = item["url"]
+            self.logger.info("Extracted article: %s, URL: %s", item["title"], article_url)
             if article_url:
-                yield response.follow(url=article_url, callback=self.parse_article, meta={"item": item})
+                response.follow(url=article_url, callback=self.parse_article, meta={"item": item})
 
         next_page = response.css("div.nav-links > a.next.page-numbers::attr(href)").get()
         if next_page:
@@ -24,7 +25,7 @@ class MydailyjuiceSpider(scrapy.Spider):
             self.logger.info(
                 f"Navigating to next page with URL {next_page_url}."
             )
-            yield scrapy.Request(url=next_page_url, callback=self.parse, errback=self.log_error)
+            yield scrapy.Request(url=next_page_url, callback=self.parse)#, errback=self.log_error
 
 
     def parse_article(self, response):
@@ -32,4 +33,5 @@ class MydailyjuiceSpider(scrapy.Spider):
         item["body"] = response.css('div[class="clearfix"] > p::text, div[class="clearfix"] > a::attr(href)').getall()
         item["tag"] = response.css('div.post-header > ul > li:nth-child(2) > a[rel="category tag"]::text').getall()
         item["date"] = response.css("div.post-header > ul > li:nth-child(3)::text").get()
+        
         yield item
